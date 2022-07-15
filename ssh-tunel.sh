a01235c23a8eaec781e3a7582c9f8692a6ffcf0d
striver619 <51977051+striver619@users.noreply.github.com>
2022-07-16 06:28:47 +0800

    Update ssh-tunel.sh

--- ssh-tunel.sh
+++ ssh-tunel.sh
@@ -4,3 +4,7 @@
 
 ps -ef|grep -v "grep"|grep "StrictHostKeyChecking"|awk '{print $2}'|xargs kill -9
 ssh -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes -o TCPKeepAlive=yes -Nf -R 6666:localhost:22 account@public_IP -p 2222
+
+# crontab 定时任务
+# 每30分钟执行一次
+# */30 * * * * /home/k8s/bin/sd.sh > /dev/null 2>&1 &
