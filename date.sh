454979d1e74be839e1bdaf94159b5e472ea8e0de
striver619 <51977051+striver619@users.noreply.github.com>
2022-10-14 16:32:43 +0800

    format date

---
+++ date.sh
@@ -0,0 +1,29 @@
+# 2022-10-14
+date +%F
+
+# 2022-10-13
+date -d -1day +%F
+
+# 2022-10-12
+date -d -2day +%F
+
+# 2022-10-15
+date -d 1day +%F
+
+# 2022-10-16
+date -d 2day +%F
+
+# 20221014
+date +%Y%m%d
+
+# 20221013
+date -d -1day +%Y%m%d
+
+# 20221012
+date -d -2day +%Y%m%d
+
+# 20221015
+date -d 1day +%Y%m%d
+
+# 20221016
+date -d 2day +%Y%m%d
