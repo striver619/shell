7600fd689ebd588dbcba3db67d7fdf845247c876
striver619 <sdzqcainiforever@163.com>
2025-06-05 11:09:46 +0800

    add cp-backup sh

---
+++ sh/xscp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# to fix no rsync env, by scp replace
+
+hosts=(
+hostname1
+hostname2
+hostname3
+)
+
+for host in ${hosts[@]}
+do
+    echo "-------- $host --------"
+    scp $1 $host:$2
+    echo ""
+done
